26c61f26dc727ea59c2e32cad5fdca54308bfadd
a13237009879 <1194128701@qq.com>
2018-08-06 23:45:29 +0800

    RPC文章接口完善

--- blog-server-service/blog-biz/src/main/java/com/blog/server/biz/ArticleBiz.java
+++ blog-server-service/blog-biz/src/main/java/com/blog/server/biz/ArticleBiz.java
@@ -1,5 +1,7 @@
 package com.blog.server.biz;
 
+import java.util.List;
+
 import com.blog.server.model.Article;
 import com.lzl.base.biz.BaseBiz;
 /**
@@ -9,5 +11,10 @@ import com.lzl.base.biz.BaseBiz;
  */
 public interface ArticleBiz extends BaseBiz<Article, Integer>
 {
-
+	/**
+	 * 得到热门的count篇文章
+	 * @param count
+	 * @return
+	 */
+	List<Article> queryHotArticle(Integer count);
 }
